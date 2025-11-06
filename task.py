3935abd7023e6fcf35001b2b8e697062f3e3da64
Igor Bondarenko <bondarenko200021@mail.ru>
2025-11-06 17:33:40 +0700

    practice with dunders

--- task.py
+++ task.py
@@ -1,3 +1,7 @@
+from functools import total_ordering
+
+
+@total_ordering
 class Task:
     def __init__(self, name: str, priority: int = 1):
         self.name = name
@@ -6,14 +10,28 @@ class Task:
     def __str__(self):
         return f"Задача - {self.name}, приоритет задачи - {self.priority}"
 
+    def _get_priority(self, other):
+        if isinstance(other, Task):
+            return other.priority
+        elif isinstance(other, int):
+            return other
+        else:
+            raise ValueError("Для сравнения нужно передать Task или int")
+
+    def __eq__(self, other):
+        return self.priority == self._get_priority(other)
+
     def __lt__(self, other):
-        if not isinstance(other, Task):
-            raise TypeError("Можно сравнивать только объекты класса Task")
-        return self.priority < other.priority
+        return self.priority < self._get_priority(other)
 
 
 t1 = Task("Сделать отчет", 5)
 t2 = Task("Купить молоко", 3)
 t3 = Task("Сделать домашнюю работу", 4)
 
-print(t1 > t3)
+print(t1 < 1)
+print(1 < t1)
+# print(t2 > 5.5) Ошибка тк сравниваем объект и float
+print(t2 > t3)
+print(t1 == t3)
+print(t1 >= t3)
