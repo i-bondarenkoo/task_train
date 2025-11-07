22318f00bbf2312c3f10f539b053414e2815b5c2
Igor Bondarenko <bondarenko200021@mail.ru>
2025-11-07 17:08:49 +0700

    123

--- task.py
+++ task.py
@@ -1,4 +1,11 @@
 from functools import total_ordering
+from enum import Enum
+
+
+class TaskStatus(str, Enum):
+    new = "new"
+    done = "done"
+    canceled = "canceled"
 
 
 @total_ordering
@@ -6,9 +13,14 @@ class Task:
     def __init__(self, name: str, priority: int = 1):
         self.name = name
         self.priority = priority
+        self._status = TaskStatus.new
 
     def __str__(self):
-        return f"Задача - {self.name}, приоритет задачи - {self.priority}"
+        return f"Задача - {self.name}, приоритет задачи - {self.priority}, статус задачи - {self._status.value}"
+
+    def change_status(self, new_status: str):
+        if self._status == TaskStatus.done:
+            return f"Статус задачи - выполнена, его нельзя изменить"
 
     def _get_priority(self, other):
         if isinstance(other, Task):
@@ -29,9 +41,10 @@ t1 = Task("Сделать отчет", 5)
 t2 = Task("Купить молоко", 3)
 t3 = Task("Сделать домашнюю работу", 4)
 
-print(t1 < 1)
-print(1 < t1)
-# print(t2 > 5.5) Ошибка тк сравниваем объект и float
-print(t2 > t3)
-print(t1 == t3)
-print(t1 >= t3)
+print(t1)
+# print(t1 < 1)
+# print(1 < t1)
+# # print(t2 > 5.5) Ошибка тк сравниваем объект и float
+# print(t2 > t3)
+# print(t1 == t3)
+# print(t1 >= t3)
