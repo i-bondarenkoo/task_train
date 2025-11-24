7cd5f0be4d79f099c419cec9152922e8c30caf46
Igor Bondarenko <bondarenko200021@mail.ru>
2025-11-24 20:35:54 +0700

    async class

--- ex3/dec.py
+++ ex3/dec.py
@@ -1,12 +1,26 @@
 import time
+import inspect
 
 
 def count_time(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        stop_time = time.time() - start_time
-        print(f"Время работы запроса - {stop_time}")
-        return result
+    if inspect.iscoroutinefunction(func):
+        # ДЛЯ АСИНХРОННОЙ ФУНКЦИИ
+        async def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = await func(*args, **kwargs)
+            stop_time = time.time() - start_time
+            print(f"Время работы запроса - {stop_time}")
+            return result
 
-    return wrapper
+        return wrapper
+
+    else:
+        # ДЛЯ СИНХРОННОЙ ФУНКЦИИ
+        def wrapper(*args, **kwargs):
+            start_time = time.time()
+            result = func(*args, **kwargs)
+            stop_time = time.time() - start_time
+            print(f"Время работы запроса - {stop_time}")
+            return result
+
+        return wrapper
--- ex3/main.py
+++ ex3/main.py
@@ -2,13 +2,22 @@ from client import Client
 from hacker_news import HackerNews
 from news import News
 from hacker_news_async import HackerNewsAsync
+import asyncio
 
-if __name__ == "__main__":
+
+async def main():
     hacker_news = HackerNews("https://hacker-news.firebaseio.com")
     # print(hacker_news.get_list_ids())
     # print("-----")
     hacker_news.get_news(25)
     print(hacker_news)
     hacker_news_async = HackerNewsAsync("https://hacker-news.firebaseio.com")
-    hacker_news_async.get_news(25)
+    await hacker_news_async.async_client.create_session()
+    await hacker_news_async.get_news(25)
     print(hacker_news_async)
+
+    await hacker_news_async.async_client.close()
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
